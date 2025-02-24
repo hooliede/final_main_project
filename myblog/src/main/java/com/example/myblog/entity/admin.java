3f07ce7057058b3f05e75b85f0b076b2542c74ae
Jiman Kim <iojjdwn@gmail.com>
2025-02-25 03:54:27 +0900

    second commit

---
+++ myblog/src/main/java/com/example/myblog/entity/admin.java
@@ -0,0 +1,15 @@
+package com.example.myblog.entity;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class admin {
+	
+	@Id
+	private long adminId;
+	
+	
+
+}
